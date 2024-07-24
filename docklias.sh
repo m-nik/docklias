2483bb250b4d8b98868c13baa8ea1d6a0e29b7b9
m-nik <62842056+m-nik@users.noreply.github.com>
2024-07-24 10:40:13 +0330

    Update docklias.sh

--- docklias.sh
+++ docklias.sh
@@ -4,20 +4,18 @@
 mkdir ~/.docker 2>/dev/null | true
 
 
-echo "alias dps="docker ps"
-alias dimg="docker images"
-alias dlog="docker logs -f"
-alias dstop="docker stop"
-alias dsrt="docker start"
-alias drst="docker restart"
-alias drm="docker rm"
-alias dri="docker rmi"
-alias dcu="docker compose up -d"
-alias dcd="docker compose down"
-alias dex="docker exec -it"
+echo "alias dps=\"docker ps\"
+alias dimg=\"docker images\"
+alias dlog=\"docker logs -f\"
+alias dstop=\"docker stop\"
+alias dsrt=\"docker start\"
+alias drst=\"docker restart\"
+alias drm=\"docker rm\"
+alias dri=\"docker rmi\"
+alias dcu=\"docker compose up -d\"
+alias dcd=\"docker compose down\"
+alias dex=\"docker exec -it\"
 " > ~/.docker/.docklias
-
 
 grep .docklias ~/.bashrc >/dev/null || echo "source ~/.docker/.docklias" >> ~/.bashrc
 
-source ~/.docker/.docklias
