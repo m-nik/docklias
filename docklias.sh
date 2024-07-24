4cf7e939556c9fbc7d9a62e8348c3113f12937d9
m-nik <62842056+m-nik@users.noreply.github.com>
2024-07-24 10:30:27 +0330

    Create docklias.sh

---
+++ docklias.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+mkdir ~/.docker 2>/dev/null | true
+
+
+echo "alias dps="docker ps"
+alias dimg="docker images"
+alias dlog="docker logs -f"
+alias dstop="docker stop"
+alias dsrt="docker start"
+alias drst="docker restart"
+alias drm="docker rm"
+alias dri="docker rmi"
+alias dcu="docker compose up -d"
+alias dcd="docker compose down"
+alias dex="docker exec -it"
+" > ~/.docker/.docklias
+
+
+grep .docklias ~/.bashrc >/dev/null || echo "source ~/.docker/.docklias" >> ~/.bashrc
+
+source ~/.docker/.docklias
